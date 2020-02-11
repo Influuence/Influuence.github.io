a918260259bc359496b141c0f4112627f813e6ce
Liliana Costa <influuencedw@gmail.com>
2020-02-11 18:57:45 -0300

    Funcionalidades JS adicionadas

--- js/components.js
+++ js/components.js
@@ -27,3 +27,45 @@
         }
     }
 })();
+
+// Menu Mobile
+(function(){
+    var $menuBtn =  document.querySelector('[data-menu="menu-button"]');
+    var $menuMobile = document.querySelector('[data-menu="menu-mobile"]');
+
+    if($menuBtn && $menuMobile){
+        $menuBtn.addEventListener('click', function(){
+            this.classList.toggle('animate');
+            $menuMobile.classList.toggle('visible');
+        });
+        
+        $menuMobile.addEventListener('click', function(e){
+            if(e.target.nodeName === 'A'){
+                $menuBtn.classList.remove('active');
+                this.classList.remove('animate');
+            }
+        });
+
+    } 
+    
+})();
+
+// Dropdown menu
+(function(){
+	const buttonDropdown = document.querySelector('[data-dropclick="dropdown-button"]');
+	const dropdown = document.querySelector('[data-dropdown="dropdown"]');
+    const classAtcive = "active";
+    const rotateClass = 'rotate'
+
+	if(buttonDropdown && dropdown){
+		buttonDropdown.addEventListener('click', function(){
+            if(!buttonDropdown.classList.contains(rotateClass) && !dropdown.classList.contains(classAtcive)){
+                buttonDropdown.classList.add(rotateClass);
+                dropdown.classList.add(classAtcive);
+            }else {
+                buttonDropdown.classList.remove(rotateClass);
+                dropdown.classList.remove(classAtcive);
+            }
+		});
+	}
+})();
