65c2e854315a377f89d5d39ccfc58a24d3197752
Liliana Costa <influuencedw@gmail.com>
2020-02-16 10:50:13 -0300

    CRemoção de tag base, reparo em funççoes JS e adição de scroll suave

--- js/components.js
+++ js/components.js
@@ -66,6 +66,74 @@
                 buttonDropdown.classList.remove(rotateClass);
                 dropdown.classList.remove(classAtcive);
             }
-		});
+		}, false);
 	}
+})();
+
+// Scroll Suave
+(function(){
+    const clickScroll = document.querySelector('a[href^="#servicos"]');
+
+    clickScroll.addEventListener('click', scrollToIdOnClick);
+
+    function scrollToIdOnClick(e){
+        e.preventDefault();
+        const to = getScrollTopByHref(e.target);
+    
+        scrollToPosition(to);
+    }
+    
+    function scrollToPosition(to){
+        // window.scroll({
+        //     top: to,
+        //     behavior: "smooth"
+        // });
+    
+        smoothScrollTo(0, to);
+    }
+    
+    function getScrollTopByHref(element){
+        const id = element.getAttribute('href');
+        return document.querySelector(id).offsetTop;
+    }
+    
+    // Função scroll suave para navegadores antigos
+    function smoothScrollTo(endX, endY, duration) {
+        const startX = window.scrollX || window.pageXOffset;
+        const startY = window.scrollY || window.pageYOffset;
+        const distanceX = endX - startX;
+        const distanceY = endY - startY;
+        const startTime = new Date().getTime();
+      
+        duration = typeof duration !== 'undefined' ? duration : 400;
+      
+        // Easing function
+        const easeInOutQuart = (time, from, distance, duration) => {
+          if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+          return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+        };
+      
+        const timer = setInterval(() => {
+          const time = new Date().getTime() - startTime;
+          const newX = easeInOutQuart(time, startX, distanceX, duration);
+          const newY = easeInOutQuart(time, startY, distanceY, duration);
+          if (time >= duration) {
+            clearInterval(timer);
+          }
+          window.scroll(newX, newY);
+        }, 1000 / 60); // 60 fps
+      };
+})();
+
+// Desabilitandp Botão
+(function(){
+    const buttonDistabled = document.querySelectorAll('.disabled');
+
+    if(buttonDistabled.length){
+        buttonDistabled.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+            })         
+        });
+    }
 })();
